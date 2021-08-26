9b19f58f2d1ef1ab3b4e603e52dccb1cb0a16326
Shuhad <shuhadz@hotmail.com>
2021-08-26 14:49:33 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+import * as my_dongle from 'bleuio'
+document.getElementById('connect').addEventListener('click', function(){
+  my_dongle.at_connect()
+})
+document.getElementById('deviceinfo').addEventListener('click', function(){
+  my_dongle.ati().then((data)=>console.log(data))
+})
+document.getElementById('central').addEventListener('click', function(){
+    my_dongle.at_central().then((data)=>console.log(data))
+})
+document.getElementById('scan').addEventListener('click', function(){
+  var showhidescanning = document.getElementById("scanningProgress");
+  showhidescanning.style.display = "block";
+  var table = document.getElementById('outputTable');
+  var tr=''
+  table.innerHTML = tr;
+my_dongle.at_gapscan(10).then((data)=>{
+  //console.log(data)
+  
+   tr += "<table class='table table-striped'>";
+  for (var i = 0; i < data.length; i++) {
+      tr += "<tr><td>" + data[i] + "</td></tr>";    
+  }
+   tr += "</table>";
+  table.innerHTML = tr;
+  showhidescanning.style.display = "none";
+
+})
+})
+
+document.getElementById('stopProcess').addEventListener('click', function(){
+   console.log(my_dongle.stop()) 
+})
+
