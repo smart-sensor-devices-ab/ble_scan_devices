6921cdaaca51d3f98177badd940a2d9d10c15f11
Shuhad <shuhadz@hotmail.com>
2021-08-27 11:54:20 +0200

    find scan data added

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import * as my_dongle from 'bleuio'
 document.getElementById('connect').addEventListener('click', function(){
   my_dongle.at_connect()
 })
-document.getElementById('deviceinfo').addEventListener('click', function(){
+/* document.getElementById('deviceinfo').addEventListener('click', function(){
   my_dongle.ati().then((data)=>console.log(data))
-})
+}) */
 document.getElementById('central').addEventListener('click', function(){
     my_dongle.at_central().then((data)=>console.log(data))
 })
@@ -15,6 +15,26 @@ document.getElementById('scan').addEventListener('click', function(){
   var tr=''
   table.innerHTML = tr;
 my_dongle.at_gapscan(10).then((data)=>{
+  //console.log(data)
+  
+   tr += "<table class='table table-striped'>";
+  for (var i = 0; i < data.length; i++) {
+      tr += "<tr><td>" + data[i] + "</td></tr>";    
+  }
+   tr += "</table>";
+  table.innerHTML = tr;
+  showhidescanning.style.display = "none";
+
+})
+})
+
+document.getElementById('findscandata').addEventListener('click', function(){
+  var showhidescanning = document.getElementById("scanningProgress");
+  showhidescanning.style.display = "block";
+  var table = document.getElementById('outputTable');
+  var tr=''
+  table.innerHTML = tr;
+my_dongle.at_findscandata('',100).then((data)=>{
   //console.log(data)
   
    tr += "<table class='table table-striped'>";
